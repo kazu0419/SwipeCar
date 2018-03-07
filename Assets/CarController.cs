2ecc63a7545217cd9668d0a9710aa809229b547e
kazu0419 <sumalove.1214@gmail.com>
2018-03-07 12:40:41 +0900

    音声を追加

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -15,12 +15,20 @@ public class CarController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// スワイプの長さを求める
 		if(Input.GetMouseButtonDown(0)){
+			// マウスをクリックした座標
 			this.startPos = Input.mousePosition;
 		} else if(Input.GetMouseButtonUp(0)){
+			// マウスを離した座標
 			Vector2 endPos = Input.mousePosition;
 			float swipeLength = endPos.x - this.startPos.x;
+
+			// スワイプの長さを初速度に変換する
 		    this.speed = swipeLength / 500.0f;
+
+			// 効果音再生
+			GetComponent<AudioSource>().Play();
 		}
 
 
